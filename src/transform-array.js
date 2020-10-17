d5fe174a6e7938eab42773623c368230593029f3
Khakhlova <khokhlova.katherina@gmail.com>
2020-10-17 21:36:17 +0300

    feat: update transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) {
+        throw new Error;
+    }
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++)
+        if (arr[i] === '--discard-prev') {
+            if (i == 0) {
+                newArr = [];
+            } else if(arr[i-2] !== '--discard-next'){
+                newArr.pop();
+            };
+
+        } else if (arr[i] === '--double-prev') {
+        if (i == 0) {
+            newArr = [];
+        } else if (arr[i-2] !== '--discard-next'){
+            newArr.push(arr[i-1]);
+        };
+    } else if (arr[i] === '--double-next') {
+        if (i == arr.length - 1) {
+            return newArr;
+        } else {
+            newArr.push(arr[i + 1]);
+        };
+    } else if (arr[i] === '--discard-next') {
+        if (i == arr.length - 1) {
+            return newArr;
+        } else {
+            i = i + 1;
+        };
+    } else {
+        newArr.push(arr[i]);
+    };
+
+    return newArr;
+
 };
