6d3c6228c0d49673846992a2476b9d68655ad0ca
Khakhlova <khokhlova.katherina@gmail.com>
2020-10-19 21:41:30 +0300

    feat: update simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    array: [],
+    getLength() {
+        return this.array.length;
+
+    },
+    addLink(value) {
+        this.array.push(value);
+        return this;
+
+
+    },
+    removeLink(position) {
+        if (position > this.array.length - 1 || typeof (position) != 'number') {
+            this.array = [];
+            throw new Error;
+        } else {
+            this.array.splice(position - 1, 1);
+            return this;
+        }
+
+    },
+    reverseChain() {
+        this.array.reverse();
+        return this;
+    },
+    finishChain() {
+        for (let i = 0; i < this.array.length; i++) {
+            this.array[i] = `( ${this.array[i]} )`;
+        };
+        let strChain = this.array.join('~~');
+        this.array = [];
+        return strChain;
+
+    }
 };
 
 module.exports = chainMaker;
