66808046ed84101c20dba3d9752db8b30430449c
Khakhlova <khokhlova.katherina@gmail.com>
2020-10-21 03:22:43 +0300

    feat: update extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+
+    str = String(str);
+    let {
+        repeatTimes = 1,
+            separator = '+',
+            addition = '',
+            additionRepeatTimes = 1,
+            additionSeparator = '',
+    } = options;
+
+    addition = String(addition);
+    separator = separator.toString();
+
+
+    let result = [];
+
+    for (let i = 0; i < repeatTimes; i++) {
+        let newStr = '';
+        let addStr = addition;
+
+        for (let j = 0; j < additionRepeatTimes - 1; j++) {
+            addStr = addStr + additionSeparator + addition;
+        }
+
+        newStr = str + addStr;
+        result.push(newStr);
+        newStr = '';
+    }
+
+    return result.join(separator);
+
 };
-  
